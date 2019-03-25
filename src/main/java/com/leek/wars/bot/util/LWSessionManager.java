42572e7311909e9b9561eedfe7a16583ce66a321
Victor Lewon <viclew@gmail.com>
2019-03-25 16:52:43 +0100

    Maj nom de la classe abstraite de gestion de session + pom réduit

--- src/main/java/com/leek/wars/bot/util/LWSessionManager.java
+++ src/main/java/com/leek/wars/bot/util/LWSessionManager.java
@@ -3,9 +3,9 @@ package com.leek.wars.bot.util;
 import com.leek.wars.client.entities.responses.SessionResponse;
 import com.leek.wars.client.util.rest.RequestProcessor;
 
-import fr.lewon.bot.http.SessionManager;
+import fr.lewon.bot.http.AbstractSessionManager;
 
-public class LWSessionManager extends SessionManager<SessionResponse> {
+public class LWSessionManager extends AbstractSessionManager<SessionResponse> {
 
 	public LWSessionManager(String login, String password) {
 		super(login, password, 3600 * 2 * 1000L);
